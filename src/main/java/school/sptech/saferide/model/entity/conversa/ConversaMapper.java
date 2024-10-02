faa1fafe057140b982bd5dfccbe91caf769095bc
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2024-10-02 19:37:18 -0300

    feat: listagem de mensagens nas conversas do responsavel

--- src/main/java/school/sptech/saferide/model/entity/conversa/ConversaMapper.java
+++ src/main/java/school/sptech/saferide/model/entity/conversa/ConversaMapper.java
@@ -1,13 +1,5 @@
 package school.sptech.saferide.model.entity.conversa;
 
-import jakarta.persistence.Tuple;
-import jakarta.persistence.TupleElement;
-import school.sptech.saferide.model.entity.mensagem.Mensagem;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConversaMapper {
@@ -19,24 +11,19 @@ public class ConversaMapper {
         dto.setResponsavel(ConversaResponse.Usuario.builder()
                 .id(entity.getResponsavel().getId())
                 .nome(entity.getResponsavel().getNome())
-                .telefone(entity.getResponsavel().getTelefone())
                 .build());
         dto.setMotorista(ConversaResponse.Usuario.builder()
                 .id(entity.getMotorista().getId())
                 .nome(entity.getMotorista().getNome())
-                .telefone(entity.getMotorista().getTelefone())
                 .build());
         if (entity.getMensagens() != null) {
             dto.setMensagems(entity.getMensagens().stream()
                     .map(s -> ConversaResponse.Mensagem.builder()
                             .id(s.getId())
-                            .data(s.getData())
-                            .status(s.getStatus())
-                            .usuarioId(s.getUsuario().getId())
-                            .dependente(ConversaResponse.Mensagem.Dependente.builder()
-                                    .id(s.getDependente().getId())
-                                    .nome(s.getDependente().getNome())
-                                    .build())
+                            .horario(s.getData())
+                            .status(s.getStatus().exibicao)
+                            .tipoUsuario(s.getUsuario().getTipo())
+                            .nome(s.getDependente().getNome())
                             .build())
                     .collect(Collectors.toList()));
         }
@@ -47,20 +34,4 @@ public class ConversaMapper {
         if (dto == null) return null;
         return new Conversa();
     }
-
-//    public static List<ListarConversasMotorista> toListarConversasMotoristaDto(Tuple tuple) {
-//        List<ListarConversasMotorista> lista = new ArrayList<>();
-//        for (Tuple t : tuple) {
-//            ListarConversasMotorista dto = new ListarConversasMotorista();
-//            dto.setId((Integer) t.get("id"));
-//            dto.setFoto((String) t.get("foto"));
-//            dto.setNome((String) t.get("nome"));
-//            dto.setMensagem((Mensagem) t.get("mensagem"));
-//            dto.setLocalDateTime((LocalDateTime) t.get("horario"));
-//            dto.setQuantidadeMensagens((Integer) t.get("qtdMensagens"));
-//
-//            lista.add(dto);
-//        }
-//        return lista;
-//    }
 }

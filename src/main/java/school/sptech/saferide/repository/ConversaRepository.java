2ef9a7b80edb37a820ed13468b8a88dc97e5a58a
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2024-10-06 23:39:21 -0300

    refactor: refatoracao do retorno das mensagens para poder suportar mensagens serem marcadas como 'lidas'

--- src/main/java/school/sptech/saferide/repository/ConversaRepository.java
+++ src/main/java/school/sptech/saferide/repository/ConversaRepository.java
@@ -11,21 +11,25 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ConversaRepository extends JpaRepository<Conversa, Integer> {
-    Optional<Conversa> findByResponsavelIdAndMotoristaId(int responsavelId, int motoristaId);
+    @Query(value = """
+    select c
+    from Conversa as c
+    where c.responsavel.id = :responsavelId
+    and c.motorista.id = :motoristaId
+    """)
+    Optional<Conversa> findByResponsavelIdAndMotoristaId(@Param("responsavelId") int responsavelId, @Param("motoristaId") int motoristaId);
 
     @Query(value = """
     select new school.sptech.saferide.model.entity.conversa.ListarConversasMotorista(
-        mt.id, mt.imagem.caminho, mt.nome, m.status, m.data, count(m.status))
+        mt.id, mt.imagem.caminho, mt.nome,
+        (select m3.status from Mensagem m3 where m3.conversa = c order by m3.data desc limit 1),
+        (select max(m2.data) from Mensagem m2 where m2.conversa = c),
+        (select count(m4.id) from Mensagem m4 where m4.conversa = c and m4.lida = false))
     from Usuario mt
     join Conversa c on mt = c.motorista
-    join Mensagem m on c = m.conversa
+    join Mensagem m on c.id = m.conversa.id
     where c.responsavel.id = :responsavelId
-      and m.data = (
-          select max(m2.data)
-          from Mensagem m2
-          where m2.conversa = c
-      )
-    group by mt.id, mt.imagem.caminho, mt.nome, m.status, m.data
+    group by mt.id, mt.imagem.caminho, mt.nome
 """)
     List<ListarConversasMotorista> findByResponsavelId(@Param("responsavelId") int responsavelId);
 }

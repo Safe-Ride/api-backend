1fb8e017ecd422e8fa183b9491809a07c635e148
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2024-12-05 14:36:19 -0300

    fix: query de tempo-real

--- src/main/java/school/sptech/saferide/repository/TempoRealRepository.java
+++ src/main/java/school/sptech/saferide/repository/TempoRealRepository.java
@@ -11,9 +11,16 @@ import java.util.Optional;
 @Repository
 public interface TempoRealRepository extends JpaRepository<TempoReal, Integer> {
     @Query(value = """
-        SELECT t.* FROM tempo_real t
+        SELECT t.*
+        FROM tempo_real t
         INNER JOIN dependente d on d.motorista_id = t.id_motorista
-        WHERE d.id = :idDependente AND t.data = (SELECT MAX(data) FROM tempo_real)
+        WHERE d.id = :idDependente
+          AND t.data = (
+            SELECT MAX(t2.data)
+            FROM tempo_real t2
+            INNER JOIN dependente d2 ON d2.motorista_id = t2.id_motorista
+            WHERE d2.id = :idDependente
+          )
         """, nativeQuery = true)
     Optional<TempoReal> findLatestByDependente(@Param("idDependente") Integer idDependente);
 }
